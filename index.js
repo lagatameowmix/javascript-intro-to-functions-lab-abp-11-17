45cd2394195290a6a9d904fa19c823d224986101
Sasha V <sashav4@gmail.com>
2017-11-12 00:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log ('hello')
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) 
+  if (string === string.toLowerCase())
   return "I can't hear you!"
 }
